8f04fd961e4a45d34bf748fcc359176d1344d5b5
shmuelhalbfinger <40396822+shmuelhalbfinger@users.noreply.github.com>
2020-09-22 23:27:45 -0400

    Commit 2

--- app/src/main/java/com/example/testtipcalculator/MainActivity.java
+++ app/src/main/java/com/example/testtipcalculator/MainActivity.java
@@ -6,22 +6,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditText checkAmountValue;
     EditText partySizeValue;
     Button buttonCompute;
-    EditText fifteenPercentTipValue;
-    EditText twentyPercentTipValue;
-    EditText twentyfivePercentTipValue;
-    EditText fifteenPercentTotalValue;
-    EditText twentyPercentTotalValue;
-    EditText twentyfivePercentTotalValue;
+    TextView fifteenPercentTipValue;
+    TextView twentyPercentTipValue;
+    TextView twentyfivePercentTipValue;
+    TextView fifteenPercentTotalValue;
+    TextView twentyPercentTotalValue;
+    TextView twentyfivePercentTotalValue;
 
-    double checkAmount;
-    int partySize;
+    Double checkAmount;
+    Integer partySize;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,38 +30,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //initialize our view objects
-        checkAmountValue = (EditText) findViewById(R.id.checkAmountValue);
-        partySizeValue = (EditText) findViewById(R.id.partySizeValue);
-        buttonCompute = (Button) findViewById(R.id.buttonCompute);
-        fifteenPercentTipValue = (EditText) findViewById(R.id.fifteenPercentTipValue);
-        twentyPercentTipValue = (EditText) findViewById(R.id.twentyPercentTipValue);
-        twentyfivePercentTipValue = (EditText) findViewById(R.id.twentyfivePercentTipValue);
-        fifteenPercentTotalValue = (EditText) findViewById(R.id.fifteenPercentTotalValue);
-        twentyPercentTotalValue = (EditText) findViewById(R.id.twentyPercentTotalValue);
-        twentyfivePercentTotalValue = (EditText) findViewById(R.id.twentyfivePercentTotalValue);
+        this.checkAmountValue = (EditText) findViewById(R.id.checkAmountValue);
+        this.partySizeValue = (EditText) findViewById(R.id.partySizeValue);
+        this.buttonCompute = (Button) findViewById(R.id.buttonCompute);
+        this.fifteenPercentTipValue = (TextView) findViewById(R.id.fifteenPercentTipValue);
+        this.twentyPercentTipValue = (TextView) findViewById(R.id.twentyPercentTipValue);
+        this.twentyfivePercentTipValue = (TextView) findViewById(R.id.twentyfivePercentTipValue);
+        this.fifteenPercentTotalValue = (TextView) findViewById(R.id.fifteenPercentTotalValue);
+        this.twentyPercentTotalValue = (TextView) findViewById(R.id.twentyPercentTotalValue);
+        this.twentyfivePercentTotalValue = (TextView) findViewById(R.id.twentyfivePercentTotalValue);
 
         //allows our button to be clickable
-        buttonCompute.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                checkAmount = Double.parseDouble(checkAmountValue.getText().toString());
-                partySize = Integer.parseInt(partySizeValue.getText().toString());
+        buttonCompute.setOnClickListener(this);
+    }
 
-                //if any of the fields are empty, display a toast message
-                if (checkAmountValue.getText().toString().isEmpty() ||
-                        partySizeValue.getText().toString().isEmpty()) {
-                    Toast.makeText(MainActivity.this, "Empty or Incorrect Value(s)!", Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    fifteenPercentTipValue.setText((int) Math.round((checkAmount * 0.15) / partySize));
-                    twentyPercentTipValue.setText((int) Math.round((checkAmount * 0.20) / partySize));
-                    twentyfivePercentTipValue.setText((int) Math.round((checkAmount * 0.25) / partySize));
+    @Override
+    public void onClick(View view) {
 
-                    fifteenPercentTotalValue.setText((int) Math.round((checkAmount * 1.15) / partySize));
-                    twentyPercentTotalValue.setText((int) Math.round((checkAmount * 1.20) / partySize));
-                    twentyfivePercentTotalValue.setText((int) Math.round((checkAmount * 1.25) / partySize));
-                }
-            }
-        });
+        //if any of the fields are empty, display a toast message
+        if (checkAmountValue.getText().toString().isEmpty() ||
+                partySizeValue.getText().toString().isEmpty()) {
+            Toast.makeText(MainActivity.this, "Empty or Incorrect Value(s)!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        this.checkAmount = Double.parseDouble(checkAmountValue.getText().toString());
+        this.partySize = Integer.parseInt(partySizeValue.getText().toString());
+
+        fifteenPercentTipValue.setText((int) Math.round((checkAmount * 0.15) / partySize));
+        twentyPercentTipValue.setText((int) Math.round((checkAmount * 0.20) / partySize));
+        twentyfivePercentTipValue.setText((int) Math.round((checkAmount * 0.25) / partySize));
+
+        fifteenPercentTotalValue.setText((int) Math.round((checkAmount * 1.15) / partySize));
+        twentyPercentTotalValue.setText((int) Math.round((checkAmount * 1.20) / partySize));
+        twentyfivePercentTotalValue.setText((int) Math.round((checkAmount * 1.25) / partySize));
     }
 }
